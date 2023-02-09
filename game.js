4d2fc784ca611bcd6a4889176fcdffbae3107a88
Thiago H. Hormann <thiago.h.hormann@gmail.com>
2023-02-09 00:09:19 -0300

    Set the initial style to 250px width screens. Add a home page to define the name of the players. Add a simple mechanism to indicate who the current player is.

--- game.js
+++ game.js
@@ -1,40 +1,140 @@
-initializeGame();
+class Player {
+  constructor(name, mark, wins = 0, loses = 0) {
+    this.name = name;
+    this.mark = mark;
+    this.wins = wins;
+    this.loses = loses;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+
+  getMark() {
+    return this.mark;
+  }
+
+  setMark(mark) {
+    this.mark = mark;
+  }
+
+  getWins() {
+    return this.wins;
+  }
+
+  increaseWins() {
+    this.wins++;
+  }
+
+  getLoses() {
+    return this.loses;
+  }
+
+  increaseLoses() {
+    this.loses++;
+  }
+}
+
+class Game {
+  constructor(status = 0, currentPlayer) {
+    this.status = status;
+    this.currentPlayer = currentPlayer;
+  }
+
+  getStatus() {
+    switch (this.status) {
+      case 0:
+        return "Not Started";
+      case 1:
+        return "Playing";
+      case 2:
+        return "Finished";
+    }
+  }
+
+  setStatus(_status) {
+    this.status = _status;
+  }
+
+  getCurrentPlayer() {
+    return this.currentPlayer;
+  }
+
+  setCurrentPlayer(currentPlayer) {
+    this.currentPlayer = currentPlayer;
+  }
+}
 
 function sortMarks() {
-  let markPlayer1 = Math.random() >= 0.5 ? "x" : "o";
-  let markPlayer2 = markPlayer1 == "x" ? "o" : "x";
+  let player1Mark = Math.random() >= 0.5 ? "x" : "o";
+  let player2Mark = player1Mark == "x" ? "o" : "x";
 
-  return [markPlayer1, markPlayer2];
+  return [player1Mark, player2Mark];
 }
 
-function initializeGame(player1 = "Player 1", player2 = "Player 2") {
-  document.querySelectorAll(".square").forEach((e) => {
+const squares = document.querySelectorAll(".square");
+const scoreLeft = document.getElementsByClassName("score-left");
+const scoreRight = document.getElementsByClassName("score-right");
+
+let labelScoreLeft = scoreLeft[0].children;
+let labelScoreRight = scoreRight[0].children;
+
+function setNames(player1 = "Player 1", player2 = "Player 2") {
+  squares.forEach((e) => {
     e.innerHTML = "";
   });
-
-  let marks = sortMarks();
-  let [markPlayer1, markPlayer2] = marks;
-
-  let labelScoreLeft =
-    document.getElementsByClassName("score-left")[0].children;
-
-  let labelScoreRight =
-    document.getElementsByClassName("score-right")[0].children;
-
+  
   labelScoreLeft[0].textContent = player1;
   labelScoreRight[0].textContent = player2;
-  labelScoreLeft[1].textContent = markPlayer1.toUpperCase();
-  labelScoreRight[1].textContent = markPlayer2.toUpperCase();
 }
 
-
-
-/*
-const board = document.querySelectorAll(".square");
-
-function getClickedIndexSquare(e) {
-  e.addEventListener("click", () => {
-    console.log(e.getAttribute("data-cell-index"));
-  });
+function setMarks(player1Mark, player2Mark) {
+  labelScoreLeft[1].textContent = player1Mark.toUpperCase();
+  labelScoreRight[1].textContent = player2Mark.toUpperCase();
 }
-*/
+
+let alternation = 0;
+
+function calcAlternation(){
+  return alternation++;
+}
+
+function setCurrentPlayer() {
+  
+  if (calcAlternation() % 2 == 0) {
+    scoreLeft[0].classList.add("currentPlayer");
+    scoreRight[0].classList.remove("currentPlayer");
+  } else {
+    scoreRight[0].classList.add("currentPlayer");
+    scoreLeft[0].classList.remove("currentPlayer");
+  }
+}
+
+let verifySquare = function () {
+  this.textContent == "" ? (textContent = markPlayer) : true;
+};
+
+Array.from(squares).forEach(function (square) {
+  square.addEventListener("click", verifySquare);
+});
+
+window.onload = () => {
+  if (document.querySelector(".board") !== null) {
+    const urlParams = new URLSearchParams(window.location.search);
+    const player1Name = urlParams.get("player1");
+    const player2Name = urlParams.get("player2");
+
+    let [player1Mark, player2Mark] = sortMarks();
+
+    const player1 = new Player(player1Name, player1Mark);
+    const player2 = new Player(player2Name, player2Mark);
+
+    setNames(player1.getName(), player2.getName());
+    setMarks(player1.getMark(), player2.getMark());
+  }
+};
+
